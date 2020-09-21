5327ffaca88bb83517c92ca96d24f5286c4a3a5f
shaishav <shaishav.shah.cs@gmail.com>
2020-09-21 17:10:44 -0400

    Update type to unsigned int64

--- src/Program.fsx
+++ src/Program.fsx
@@ -8,16 +8,16 @@ open System
 
 // Utility Functions - Start
 // -------------------------
-let isValidInput (n: int64, k: int64) = 
-    n > 0L && k > 0L
+let isValidInput (n: uint64, k: uint64) = 
+    n > 0UL && k > 0UL
 
-let strToInt64 str =
-    str |> int64
+let strToUInt64 str =
+    str |> uint64
 
-let dblToInt64 dbl = 
-    dbl |> int64
+let dblToUInt64 dbl = 
+    dbl |> uint64
 
-let toDouble (int: int64) = 
+let toDouble (int: uint64) = 
     int |> double
 
 let rootOf = 
@@ -29,7 +29,7 @@ let squareOf n  =
 let isSquare n =
     let rootN = rootOf n
     let floorRootN = floor rootN
-    (rootN = floorRootN) && (dblToInt64 floorRootN * dblToInt64 floorRootN = n)
+    (rootN = floorRootN) && (dblToUInt64 floorRootN * dblToUInt64 floorRootN = n)
 // -----------------------
 // Utility Functions - End
 
@@ -37,19 +37,19 @@ let isSquare n =
 // Actor System Logic - Start
 // --------------------------
 // Create a root actor
-let system = create "system" (Configuration.load())
+let system = ActorSystem.Create "system"
 
 type TaskDetails = {
-    StartNumber: int64;
-    EndNumber: int64;
-    WindowLength: int64;
+    StartNumber: uint64;
+    EndNumber: uint64;
+    WindowLength: uint64;
 }
 
 type JobInfo = {
-    TotalTaskCount: int64;
-    WindowLength: int64;
-    TaskCountPerWorker: int64;
-    WorkerCount: int64;
+    TotalTaskCount: uint64;
+    WindowLength: uint64;
+    TaskCountPerWorker: uint64;
+    WorkerCount: uint64;
 }
 
 type Worker (name) =
@@ -63,15 +63,15 @@ type Worker (name) =
                 let last = input.EndNumber
                 let windowLen = input.WindowLength
 
-                let mutable sumOfSquares = 0L
-                for number in first .. first + windowLen - 1L do
+                let mutable sumOfSquares = 0UL
+                for number in first .. first + windowLen - 1UL do
                     sumOfSquares <- sumOfSquares + squareOf number
         
                 if isSquare sumOfSquares then
                     sender <! first
 
-                for number in (first + 1L) .. last do
-                    sumOfSquares <- sumOfSquares - squareOf (number - 1L) + squareOf (number + windowLen - 1L)
+                for number in (first + 1UL) .. last do
+                    sumOfSquares <- sumOfSquares - squareOf (number - 1UL) + squareOf (number + windowLen - 1UL)
 
                     if isSquare sumOfSquares then
                         sender <! number
@@ -83,10 +83,10 @@ type Worker (name) =
 
 
 // Helper to distribute tasks among the workers
-let rec workDistributionHelper (jobInfo: JobInfo, first: int64, workers: List<IActorRef>, workerIndex: int) = 
+let rec distributeWork (jobInfo: JobInfo, first: uint64, workers: List<IActorRef>, workerIndex: int) = 
     let last = 
         if first + jobInfo.TaskCountPerWorker < jobInfo.TotalTaskCount then 
-            first + jobInfo.TaskCountPerWorker - 1L
+            first + jobInfo.TaskCountPerWorker - 1UL
         else 
             jobInfo.TotalTaskCount
     
@@ -96,17 +96,17 @@ let rec workDistributionHelper (jobInfo: JobInfo, first: int64, workers: List<IA
         WindowLength = jobInfo.WindowLength;
     }
     
-    let index = if workerIndex |> int64 >= jobInfo.WorkerCount then 0 else workerIndex
+    let index = if workerIndex |> uint64 >= jobInfo.WorkerCount then 0 else workerIndex
     workers |> List.item index <! taskDetails
 
     if last < jobInfo.TotalTaskCount then
-        workDistributionHelper (jobInfo, last + 1L, workers, workerIndex + 1)
+        distributeWork (jobInfo, last + 1UL, workers, workerIndex + 1)
 
 
 type Supervisor (name) = 
     inherit Actor()
-    let mutable finishedWorkerCount = 0L
-    let mutable totalWorkerCount = 0L
+    let mutable finishedWorkerCount = 0UL
+    let mutable totalWorkerCount = 0UL
     let mutable parent: IActorRef = null
     
     override x.OnReceive(message: obj) = 
@@ -118,16 +118,16 @@ type Supervisor (name) =
                     parent <- sender
 
                     let workers = 
-                        [1L .. input.WorkerCount]
+                        [1UL .. input.WorkerCount]
                         |> List.map(fun id ->   let properties = [| "worker_" + (id |> string) :> obj |]
                                                 system.ActorOf(Props(typedefof<Worker>, properties)))
                     
-                    workDistributionHelper (input, 1L, workers, 0)
-                | :? int64 as result -> 
+                    distributeWork (input, 1UL, workers, 0)
+                | :? uint64 as result -> 
                     printfn "%A" result
                 | :? string -> 
                     sender <! PoisonPill.Instance
-                    finishedWorkerCount <- finishedWorkerCount + 1L
+                    finishedWorkerCount <- finishedWorkerCount + 1UL
                     if finishedWorkerCount = totalWorkerCount then
                         parent <! "Finish!"
                 | _ -> 
@@ -138,17 +138,17 @@ type Supervisor (name) =
 
 
 // Main driver function
-let main (n: int64, k: int64) =
+let main (n: uint64, k: uint64) =
     // Input validation
     if not (isValidInput (n, k)) then 
         printfn "Error: Invalid Values for N and/or K."
     else 
         // Configuration for total number of workers
-        let numberOfWorkers = Environment.ProcessorCount |> int64
+        let numberOfWorkers = Environment.ProcessorCount |> uint64
         
         // Amount of work to be done by a single worker
         let taskCountPerWorker = 
-            if n <= numberOfWorkers then 1L 
+            if n <= numberOfWorkers then 1UL 
             else n / numberOfWorkers
 
         let supervisor = system.ActorOf(Props(typedefof<Supervisor>, [| "supervisor" :> obj |]))
@@ -161,7 +161,7 @@ let main (n: int64, k: int64) =
         }
         
         let task = supervisor <? jobInfo
-        let response = Async.RunSynchronously(task)
+        Async.RunSynchronously(task) |> ignore
         supervisor <! PoisonPill.Instance
         system.Terminate() |> ignore
 
@@ -169,7 +169,7 @@ let main (n: int64, k: int64) =
 // Read command line inputs and pass on to the driver function
 match fsi.CommandLineArgs with
     | [|_; n; k|] -> 
-        let nVal = strToInt64 n
-        let kVal = strToInt64 k
+        let nVal = strToUInt64 n
+        let kVal = strToUInt64 k
         main (nVal, kVal)
     | _ -> printfn "Error: Invalid Arguments. N and K values must be passed."
